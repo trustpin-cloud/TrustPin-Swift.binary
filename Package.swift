8654612badca1a94140b042e5cf9512cc639013d
TrustPin <trustpin-cloud@proton.me>
2025-07-18 16:01:01 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.1
+import PackageDescription
+
+let package = Package(
+    name: "TrustPinKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v13),
+        .macCatalyst(.v13),
+        .watchOS(.v7),
+        .tvOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "TrustPinKit",
+            targets: ["TrustPinKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TrustPinKit",
+            url: "https://github.com/trustpin-cloud/TrustPin-Swift.binary/releases/download/0.1.0/TrustPinKit-0.1.0.xcframework.zip",
+            checksum: "c62c99ccaf9ccd87824a24993c068981a9efb3d8b32e6e4cde8b9982dd11f630"
+        )
+    ]
+)
