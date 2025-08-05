89052182a4f80ed5ad06b052fbe04db347ee6ea6
TrustPin <trustpin-cloud@proton.me>
2025-08-05 14:33:02 +0000

    Release 0.25.0

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrustPinKit",
-            url: "https://github.com/trustpin-cloud/TrustPin-Swift.binary/releases/download/0.23.0/TrustPinKit-0.23.0.xcframework.zip",
-            checksum: "752e5aa272c85aab78c2a682910804b1df86ddf23c170a919d486293c8ab9513"
+            url: "https://github.com/trustpin-cloud/TrustPin-Swift.binary/releases/download/0.25.0/TrustPinKit-0.25.0.xcframework.zip",
+            checksum: "7e4d093e8b203ccacb4492cd960c4725368d519a147a67d07f5dd77302ac974b"
         )
     ]
 )
